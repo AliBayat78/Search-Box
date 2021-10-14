ade4e0a6b259ea8d7270fc25e3427f80ce1775ff
Ali Bayat <ali.bayat77@yahoo.com>
2021-10-14 16:55:46 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', function () {
+  container.classList.toggle('active')
+})
